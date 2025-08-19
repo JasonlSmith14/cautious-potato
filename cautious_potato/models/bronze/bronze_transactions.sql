28d3861291d84182d5f31e23b5f58bbaf4df5481
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-08-19 16:12:26 +0200

    Include category information

--- cautious_potato/models/bronze/bronze_transactions.sql
+++ cautious_potato/models/bronze/bronze_transactions.sql
@@ -1,2 +1,13 @@
-SELECT *
+SELECT 
+    transactions.id,
+    statement_id, 
+    transaction_date,
+    amount,
+    description,
+    description_embedding,
+    cleaned_description,
+    category,
+    reasoning,
+    balance
 FROM transactions
+JOIN {{ ref('bronze_categories') }} AS bronze_categories ON transactions.category_id = bronze_categories.id
