52d675944a08be9d5f1880848246933f32a6e1e1
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-08-19 16:10:24 +0200

    add abstract methods that are being used

--- database_service/base_database_service.py
+++ database_service/base_database_service.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import Any, Dict, List, Type, TypeVar
 
 from sqlmodel import SQLModel
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+T = TypeVar("T", bound=SQLModel)
 
 
 class BaseDatabaseService(ABC):
@@ -10,9 +13,37 @@ class BaseDatabaseService(ABC):
         pass
 
     @abstractmethod
-    def create_single(self, model: SQLModel):
+    def create_single(self, model: Type[T]):
+        pass
+
+    @abstractmethod
+    def update_single(self, model: Type[T], model_id: int, update_data: Type[T]):
         pass
 
     @abstractmethod
     def create_tables(self, tables: List[str]):
+        pass
+
+    @abstractmethod
+    def read_nulls(
+        self, model: Type[T], column: InstrumentedAttribute
+    ) -> List[Type[T]]:
+        pass
+
+    @abstractmethod
+    def read_with_filters(
+        self, model: Type[T], filters: Dict[InstrumentedAttribute, Any]
+    ) -> List[Type[T]]:
+        pass
+
+    @abstractmethod
+    def search_by_embedding(
+        self,
+        embedding: List[float],
+        embedding_column: InstrumentedAttribute,
+        model: Type[T],
+        limit: int,
+        threshold: float,
+        exclude_id: int,
+    ) -> List[T]:
         pass
