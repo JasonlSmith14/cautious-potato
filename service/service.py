5d261d4df66a085804fb2b2ab5f372c70954c092
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-08-31 22:30:31 +0200

    Increase the threshold Ensure transactions only get grouped once

--- service/service.py
+++ service/service.py
@@ -81,26 +81,37 @@ class Service:
 
     def categorise_transactions(self):
         transaction_groups: List[List[Transaction]] = []
+        transaction_group_ids = []
         uncategorised_transactions: List[Transaction] = (
             self.database_service.read_nulls(
                 model=Transaction, column=Transaction.category_id
             )
         )
         for transaction in uncategorised_transactions:
+            for transaction_group in transaction_groups:
+                transaction_group_ids += [t.id for t in transaction_group]
+
+            if transaction.id in transaction_group_ids:
+                continue
+
             similar_transactions: List[Transaction] = (
                 self.database_service.search_by_embedding(
                     embedding=transaction.description_embedding,
                     embedding_column=Transaction.description_embedding,
                     model=Transaction,
+                    threshold=0.95,
                     exclude_id=transaction.id,
                 )
             )
 
             similar_uncategorised_transactions = [
-                t for t in similar_transactions if t.category_id == None
+                t
+                for t in similar_transactions
+                if t.category_id == None and t.id not in transaction_group_ids
             ]
 
-            transaction_groups.append(similar_uncategorised_transactions)
+            if similar_uncategorised_transactions:
+                transaction_groups.append(similar_uncategorised_transactions)
 
         for transaction_group in transaction_groups:
             first_transaction = transaction_group[0]
@@ -118,7 +129,14 @@ class Service:
             ]
 
             if similar_transactions_with_categories:
-                category = similar_transactions_with_categories[0].category
+                for transaction in transaction_group:
+                    self.database_service.update_single(
+                        model=Transaction,
+                        model_id=transaction.id,
+                        update_data=similar_transactions_with_categories[0].model_dump(
+                            include={"category_id"}
+                        ),
+                    )
 
             else:
                 average_amount = sum([t.amount for t in transaction_group]) / len(
@@ -138,8 +156,8 @@ class Service:
 
                 category = Category.model_validate(category_information)
 
-            category.transactions = transaction_group
-            self.database_service.create_single(model=category)
+                category.transactions = transaction_group
+                self.database_service.create_single(model=category)
 
         uncategorised_transactions: List[Transaction] = (
             self.database_service.read_nulls(
