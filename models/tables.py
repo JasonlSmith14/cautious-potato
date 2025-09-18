1d539ba11b1520e0a72489f9ce8cd551f87a8821
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-09-18 21:56:59 +0200

    Add extend existing to tables

--- models/tables.py
+++ models/tables.py
@@ -5,7 +5,7 @@ from sqlmodel import Relationship, SQLModel, Field
 from models.information import CategoryInformation, TransactionInformation
 
 
-class ParsedStatement(SQLModel, table=True):
+class ParsedStatement(SQLModel, table=True, extend_existing=True):
     __tablename__ = "parsed_statements"
     id: Optional[int] = Field(default=None, primary_key=True)
     statement_id: Optional[int] = Field(default=None, foreign_key="statements.id")
@@ -15,7 +15,7 @@ class ParsedStatement(SQLModel, table=True):
     statement: Optional["Statement"] = Relationship(back_populates="parsed_statements")
 
 
-class Statement(SQLModel, table=True):
+class Statement(SQLModel, table=True, extend_existing=True):
     __tablename__ = "statements"
     id: Optional[int] = Field(default=None, primary_key=True)
 
@@ -28,7 +28,7 @@ class Statement(SQLModel, table=True):
     )
 
 
-class Transaction(TransactionInformation, table=True):
+class Transaction(TransactionInformation, table=True, extend_existing=True):
     __tablename__ = "transactions"
     id: Optional[int] = Field(default=None, primary_key=True)
     statement_id: Optional[int] = Field(default=None, foreign_key="statements.id")
@@ -38,7 +38,7 @@ class Transaction(TransactionInformation, table=True):
     category: Optional["Category"] = Relationship(back_populates="transactions")
 
 
-class Category(CategoryInformation, table=True):
+class Category(CategoryInformation, table=True, extend_existing=True):
     __tablename__ = "categories"
     id: Optional[int] = Field(default=None, primary_key=True)
 
